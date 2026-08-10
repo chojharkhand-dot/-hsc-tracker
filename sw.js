b2f48aad0dc8ee8a6db84ac26966e03d2f76eb14
chojharkhand-dot <cho.jharkhand@gmail.com>
2026-08-10 12:52:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,28 @@
-const CACHE='hsc-tracker-v1';const AS=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(AS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
+const CACHE='hsc-tracker-v2';
+const AS=['./','./index.html','./manifest.webmanifest'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(AS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+      ))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      return cached || fetch(event.request).catch(()=>caches.match('./index.html'));
+    })
+  );
+});
