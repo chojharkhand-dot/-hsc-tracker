27c0d10a37714e51f7c57f9b25d5f525e2571552
chojharkhand-dot <cho.jharkhand@gmail.com>
2026-08-12 14:56:04 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hsc-tracker-v12-report-controls';
+const CACHE='hsc-tracker-v13-report-print-restore';
 const AS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
